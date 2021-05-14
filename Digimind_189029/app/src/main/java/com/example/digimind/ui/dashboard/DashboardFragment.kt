79a0195d5d99c39a48c859fe27f8b250e021ed5c
AlfonsoSempoalt <alfonsosempoalt@hotmail.com>
2021-05-13 19:51:28 -0600

    firestore

--- Digimind_189029/app/src/main/java/com/example/digimind/ui/dashboard/DashboardFragment.kt
+++ Digimind_189029/app/src/main/java/com/example/digimind/ui/dashboard/DashboardFragment.kt
@@ -12,36 +12,44 @@ import androidx.lifecycle.ViewModelProviders
 import com.example.digimind.R
 import com.example.digimind.Task
 import com.example.digimind.ui.home.HomeFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
 class DashboardFragment : Fragment() {
-
+    private lateinit var storage: FirebaseFirestore
+    private lateinit var user: FirebaseAuth
     private lateinit var dashboardViewModel: DashboardViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
+        storage = FirebaseFirestore.getInstance()
+        user = FirebaseAuth.getInstance()
+
         dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
 
         val btn_time: Button = root.findViewById(R.id.btn_time)
 
 
-        btn_time.setOnClickListener{
+        btn_time.setOnClickListener {
             val cal = Calendar.getInstance()
-            val timeSetListener = TimePickerDialog.OnTimeSetListener{timePicker, hour, minute ->
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
                 cal.set(Calendar.HOUR_OF_DAY, hour)
                 cal.set(Calendar.MINUTE, minute)
 
                 btn_time.text = SimpleDateFormat("HH:mm").format(cal.time)
             }
-            TimePickerDialog(root.context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY),
-                cal.get(Calendar.MINUTE), true).show()
+            TimePickerDialog(
+                root.context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE), true
+            ).show()
 
         }
 
@@ -55,33 +63,36 @@ class DashboardFragment : Fragment() {
         val checkSaturday = root.findViewById(R.id.checkSatuday) as CheckBox
         val checkSunday = root.findViewById(R.id.checkSunday) as CheckBox
 
-        btn_save.setOnClickListener{
+        btn_save.setOnClickListener {
 
             var titulo = et_titulo.text.toString()
             var time = btn_time.text.toString()
 
-            var days = ArrayList<String>()
+            var taskHashMap = hashMapOf(
+                "actividad" to "${titulo}",
+                "tiempo" to "${time}",
+                "email" to "${user.currentUser.email}",
+                "lu" to checkMonday.isChecked,
+                "ma" to checkTuesday.isChecked,
+                "mi" to checkWednesday.isChecked,
+                "ju" to checkThursday.isChecked,
+                "vi" to checkFriday.isChecked,
+                "sa" to checkSaturday.isChecked,
+                "do" to checkSunday.isChecked
+            )
 
-            if(checkMonday.isChecked)
-                days.add("Monday")
-            if(checkTuesday.isChecked)
-                days.add("Tuesday")
-            if(checkWednesday.isChecked)
-                days.add("Wednesday")
-            if(checkThursday.isChecked)
-                days.add("Thursday")
-            if(checkFriday.isChecked)
-                days.add("Friday")
-            if(checkSaturday.isChecked)
-                days.add("Saturday")
-            if(checkSunday.isChecked)
-                days.add("Sunday")
-
-            var task = Task(titulo, days, time)
-
-            HomeFragment.tasks.add(task)
-
-            Toast.makeText(root.context, "new task added", Toast.LENGTH_SHORT).show()
+            storage.collection("actividades")
+                .add(taskHashMap)
+                .addOnSuccessListener {
+                    Toast.makeText(root.context, "se agregó una nueva tarea", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(
+                        root.context,
+                        "Ocurrio un error agregando la tarea",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
         }
 
 
--- Digimind_189029/app/src/main/java/com/example/digimind/ui/home/HomeFragment.kt
+++ Digimind_189029/app/src/main/java/com/example/digimind/ui/home/HomeFragment.kt
@@ -1,71 +1,96 @@
 package com.example.digimind.ui.home
 
 import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.GridView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.digimind.R
 import com.example.digimind.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_home.*
+
 
 class HomeFragment : Fragment() {
 
     private var adaptador: AdaptadorTareas? = null
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var storage: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
 
-    companion object{
+    companion object {
         var tasks = ArrayList<Task>()
-        var first = true
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
+
+        this.storage = FirebaseFirestore.getInstance()
+        this.auth = FirebaseAuth.getInstance()
+
         homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+            ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
-        if (first){
-            fillTasks()
-            first = false
-        }
-
-
-        Toast.makeText(root.context, tasks[0].toString(), Toast.LENGTH_LONG).show()
-        var gridView: GridView = root.findViewById(R.id.gridview)
-
-        adaptador = AdaptadorTareas(root.context, tasks)
-        gridView.adapter = adaptador
-
-
+        fillTasks(root.context)
         return root
     }
 
-    fun fillTasks(){
-        tasks.add(Task("Practice 1", arrayListOf("Tuesday"), "17:30"))
-        tasks.add(Task("Practice 2", arrayListOf("Monday", "Sunday"), "17:40"))
-        tasks.add(Task("Practice 3", arrayListOf("Wednesday"), "14:00"))
-        tasks.add(Task("Practice 4", arrayListOf("Saturday"), "11:00"))
-        tasks.add(Task("Practice 5", arrayListOf("Friday"), "13:00"))
-        tasks.add(Task("Practice 6", arrayListOf("Thursday"), "10:40"))
-        tasks.add(Task("Practice 7", arrayListOf("Monday"), "12:00"))
+    fun fillTasks(context: Context) {
+        this.storage.collection("actividades")
+            .whereEqualTo("email", auth.currentUser.email)
+            .get()
+            .addOnSuccessListener { it ->
+                it.forEach {
+                    var dias = ArrayList<String>()
 
+                    if (it.getBoolean("lu") == true) {
+                        dias.add("Monday")
+                    }
+                    if (it.getBoolean("ma") == true) {
+                        dias.add("Monday")
+                    }
+                    if (it.getBoolean("mi") == true) {
+                        dias.add("Wednesday")
+                    }
+
+                    if (it.getBoolean("ju") == true) {
+                        dias.add("Thursday")
+                    }
+
+                    if (it.getBoolean("vi") == true) {
+                        dias.add("Friday")
+                    }
+
+                    if (it.getBoolean("sa") == true) {
+                        dias.add("Saturday")
+                    }
+
+                    if (it.getBoolean("do") == true) {
+                        dias.add("Sunday")
+                    }
+                    tasks!!.add(Task(it.getString("actividad")!!, dias, it.getString("tiempo")!!))
+                }
+                adaptador = AdaptadorTareas(context, tasks)
+                gridview.adapter = adaptador
+            }
     }
 
-    private class AdaptadorTareas: BaseAdapter {
+
+    private class AdaptadorTareas : BaseAdapter {
         var tasks = ArrayList<Task>()
         var context: Context? = null
 
-        constructor(contexto: Context, tasks: ArrayList<Task>){
+        constructor(contexto: Context, tasks: ArrayList<Task>) {
             this.context = contexto
             this.tasks = tasks
         }
